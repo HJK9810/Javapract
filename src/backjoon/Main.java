23756e8c9a44fd701b72fe23de712b2803430a10
HJK9810 <ruby45342@gamail.com>
2022-01-08 16:06:19 +0900

    나머지&곱셈계산

--- src/backjoon/Main.java
+++ src/backjoon/Main.java
@@ -8,15 +8,46 @@ public class Main {
         int A = input.nextInt();
         int B = input.nextInt();
 
-        System.out.println(A+B);
+        System.out.println(A + B);
         return 0;
     }
+
     public static double Divis1() {
         Scanner input = new Scanner(System.in);
         double A = input.nextInt();
         double B = input.nextInt();
 
-        System.out.println(A/B);
+        System.out.println(A / B);
+        return 0;
+    }
+
+    public static int Remain () {
+        Scanner input = new Scanner(System.in);
+        int A = input.nextInt();
+        int B = input.nextInt();
+        int C = input.nextInt();
+
+        System.out.println((A + B) % C);
+        System.out.println(((A % C) + (B % C)) % C);
+        System.out.println((A * B) % C);
+        System.out.println(((A % C) * (B % C)) % C);
+        return 0;
+    }
+
+    public static int Multi() {
+        Scanner input = new Scanner(System.in);
+        int A = input.nextInt();
+        int B = input.nextInt();
+
+        int first = B/100;
+        B %= 100;
+        int second = B/10;
+        B%=10;
+        System.out.println(A*B);
+        System.out.println(A*second);
+        System.out.println(A*first);
+
+        System.out.println(A*first*100+A*second*10+A*B);
         return 0;
     }
     public static void main(String[] args) {
