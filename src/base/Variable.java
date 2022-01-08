ce8c49b7aa3c974298554146a72cec869313b635
HJK9810 <ruby45342@gamail.com>
2022-01-08 15:12:45 +0900

    사각형의 넓이

--- src/base/Variable.java
+++ src/base/Variable.java
@@ -32,10 +32,28 @@ public class Variable {
         return radius * radius * PI;
     }
 
+    public static double SqureArea() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("사각형의 가로를 입력하시오: ");
+        double width = input.nextDouble();
+        System.out.print("사각형의 세로를 입력하시오: ");
+        double height = input.nextDouble();
+
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+
+        System.out.println("사각형의 넓이는 " + area);
+        System.out.println("사각형의 둘레는 " + perimeter);
+
+        return 0;
+    }
+
     public static void main(String[] args) {
 //        System.out.println("걸리는 시간은"+ Lightdistance() +"광년입니다.");
 //        QudraticFormular(1, -3.0, 2.0);
-        System.out.println(CircleArea());
+//        System.out.println(CircleArea());
+        SqureArea();
     }
 
 }
