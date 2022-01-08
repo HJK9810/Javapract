64212e7a616f18f6b8a1155648ca2c1e71579ad1
HJK9810 <ruby45342@gamail.com>
2022-01-08 15:37:54 +0900

    간단 덧셈&나눗셈

--- src/backjoon/Main.java
+++ src/backjoon/Main.java
@@ -1,10 +1,25 @@
 package backjoon;
 
+import java.util.Scanner;
+
 public class Main {
+    public static int Plus1() {
+        Scanner input = new Scanner(System.in);
+        int A = input.nextInt();
+        int B = input.nextInt();
+
+        System.out.println(A+B);
+        return 0;
+    }
+    public static double Divis1() {
+        Scanner input = new Scanner(System.in);
+        double A = input.nextInt();
+        double B = input.nextInt();
+
+        System.out.println(A/B);
+        return 0;
+    }
     public static void main(String[] args) {
-        System.out.println("\\    /\\");
-        System.out.println(" )  ( ')");
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
+
     }
 }
