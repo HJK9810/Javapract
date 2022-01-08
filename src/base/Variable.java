5e9722f28d696a3f4a81db436a4ffab0c9f8b78b
HJK9810 <ruby45342@gamail.com>
2022-01-08 14:57:38 +0900

    근의공식

--- src/base/Variable.java
+++ src/base/Variable.java
@@ -9,7 +9,20 @@ public class Variable {
 
         return distance / lightyear;
     }
-    public static void main(String[] args) {
-        System.out.println("걸리는 시간은"+ Lightdistance() +"광년입니다.");
+
+    public static double QudraticFormular(double a, double b, double c) {
+//        근의공식 : (-b +- 루트(b^2 - 4ac)) / 2a
+        double x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / 2 * a;
+        double x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / 2 * a;
+
+        System.out.printf("근은 %.1f\n", x1);
+        System.out.printf("근은 %.1f\n", x2);
+        return 0;
     }
+
+    public static void main(String[] args) {
+//        System.out.println("걸리는 시간은"+ Lightdistance() +"광년입니다.");
+        QudraticFormular(1, -3.0, 2.0);
+    }
+
 }
