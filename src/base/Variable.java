db4d4640616f1171a3133aae8ea6cc1265874a2c
HJK9810 <ruby45342@gamail.com>
2022-01-08 14:40:10 +0900

    double연습-광년계산

--- src/base/Variable.java
+++ src/base/Variable.java
@@ -1,8 +1,15 @@
 package base;
 
 public class Variable {
+    public static double Lightdistance() {
+        double distance = 40e12;
+        final double lightfast = 3e5; // 빛의 속도는 변하지 않음 = 상수
 
+        double lightyear = lightfast * (60 * 60 * 24 * 365);
+
+        return distance / lightyear;
+    }
     public static void main(String[] args) {
-//        System.out.printf(solution());
+        System.out.println("걸리는 시간은"+ Lightdistance() +"광년입니다.");
     }
 }
