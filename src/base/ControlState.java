ec81f4a22ce4e73c5a0e9ee7fe7eb7fd9646a013
HJK9810 <ruby45342@gamail.com>
2022-01-09 15:49:40 +0900

    최대공약수찾기

--- src/base/ControlState.java
+++ src/base/ControlState.java
@@ -53,12 +53,28 @@ public class ControlState {
                 break;
         }
 
-        System.out.println("월의 날수는 "+ day);
+        System.out.println("월의 날수는 " + day);
+    }
+
+    public static void GCD() { // 최대공약수 찾기
+        Scanner input = new Scanner(System.in);
+        System.out.print("두개의 정수를 입력하시오(큰수, 작은수): ");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+
+        while (num2 != 0) {
+            int r = num1 % num2;
+            num1 = num2;
+            num2 = r;
+        }
+
+        System.out.println("최대 공약수는 " + num1);
     }
 
     public static void main(String[] args) {
 //        CheckGrade();
 //        PrintHello();
-        ContMonthDays();
+//        ContMonthDays();
+        GCD();
     }
 }
