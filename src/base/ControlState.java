cf407796daaed0b0430543c780871f0023fabcef
HJK9810 <ruby45342@gamail.com>
2022-01-09 14:29:12 +0900

    인사말출력(if문)

--- src/base/ControlState.java
+++ src/base/ControlState.java
@@ -19,7 +19,21 @@ public class ControlState {
         return 0;
     }
 
+    public static int PrintHello() { // 인사말 출력
+        Date date = new Date();
+        int currentHour = date.getHours();
+        System.out.println("현재시간은 " + date);
+
+        if (currentHour < 11) System.out.println("Good morning");
+        else if (currentHour < 16) System.out.println("Good afternoon");
+        else if (currentHour < 20) System.out.println("Good evening");
+        else System.out.println("Good night");
+
+        return 0;
+    }
+
     public static void main(String[] args) {
-        CheckGrade();
+//        CheckGrade();
+        PrintHello();
     }
 }
