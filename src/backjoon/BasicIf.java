57432d3dc406fca7f81d0825b61b27c235ddf5d7
HJK9810 <ruby45342@gamail.com>
2022-01-11 15:59:20 +0900

    시험성적

--- src/backjoon/BasicIf.java
+++ src/backjoon/BasicIf.java
@@ -13,7 +13,18 @@ public class BasicIf {
         else System.out.println("==");
     }
 
+    public static void TestGrade() {
+        Scanner input = new Scanner(System.in);
+        int grade = input.nextInt();
+
+        if(grade>=90) System.out.println("A");
+        else if(grade>=80) System.out.println("B");
+        else if(grade>=70) System.out.println("C");
+        else if(grade>=60) System.out.println("D");
+        else System.out.println("F");
+    }
     public static void main(String[] args) {
-        CheckTwoNum();
+//        CheckTwoNum();
+        TestGrade();
     }
 }
