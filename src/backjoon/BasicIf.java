e490d3f237c770df17fee0ef5b50a5af2094ec39
HJK9810 <ruby45342@gamail.com>
2022-01-11 16:05:37 +0900

    코드정렬

--- src/backjoon/BasicIf.java
+++ src/backjoon/BasicIf.java
@@ -17,14 +17,15 @@ public class BasicIf {
         Scanner input = new Scanner(System.in);
         int grade = input.nextInt();
 
-        if(grade>=90) System.out.println("A");
-        else if(grade>=80) System.out.println("B");
-        else if(grade>=70) System.out.println("C");
-        else if(grade>=60) System.out.println("D");
+        if (grade >= 90) System.out.println("A");
+        else if (grade >= 80) System.out.println("B");
+        else if (grade >= 70) System.out.println("C");
+        else if (grade >= 60) System.out.println("D");
         else System.out.println("F");
     }
+
     public static void main(String[] args) {
 //        CheckTwoNum();
-        TestGrade();
+//        TestGrade();
     }
 }
