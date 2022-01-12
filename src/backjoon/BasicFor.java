73ea0261f2c53e8395ebb346a44d6ed2b75825ee
HJK9810 <ruby45342@gamail.com>
2022-01-12 20:04:50 +0900

    A+B-7

--- src/backjoon/BasicFor.java
+++ src/backjoon/BasicFor.java
@@ -11,7 +11,24 @@ public class BasicFor {
             System.out.println(num + " * " + i + " = " + num * i);
         }
     }
-    public static void main(String[] args) {
 
+    public static void Plus7() {
+        Scanner input = new Scanner(System.in);
+        final int SIZE = input.nextInt();
+        int[] nums = new int[SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            int num1 = input.nextInt();
+            int num2 = input.nextInt();
+            nums[i] = num1 + num2;
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            System.out.println("Case #" + (i + 1) + ": " + nums[i]);
+        }
+    }
+    public static void main(String[] args) {
+//        NineXNine();
+        Plus7();
     }
 }
