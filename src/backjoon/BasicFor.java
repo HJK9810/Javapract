e81c5af446c10fce82439d5645a47728ebd153b3
HJK9810 <ruby45342@gamail.com>
2022-01-13 14:12:07 +0900

    X보다작은수출력

--- src/backjoon/BasicFor.java
+++ src/backjoon/BasicFor.java
@@ -27,6 +27,22 @@ public class BasicFor {
             System.out.println("Case #" + (i + 1) + ": " + nums[i]);
         }
     }
+
+    public static void AryMinNum() { // X보다 작은수
+        Scanner input = new Scanner(System.in);
+        int SIZE = input.nextInt();
+        int X = input.nextInt();
+        int[] ary = new int[SIZE];
+
+        for(int i=0;i<SIZE;i++) {
+            ary[i] = input.nextInt();
+        }
+
+        for (int num : ary) {
+            if (num < X) System.out.println(num);
+        }
+    }
+
     public static void main(String[] args) {
 //        NineXNine();
         Plus7();
