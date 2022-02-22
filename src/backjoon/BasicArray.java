ec8a8d10d93204c479a0446aaa2624f799e3d38a
HJK9810 <ruby45342@gamail.com>
2022-02-22 15:49:17 +0900

    최소최대구하기

--- src/backjoon/BasicArray.java
+++ src/backjoon/BasicArray.java
@@ -1,7 +1,27 @@
 package backjoon;
 
-public class BasicArray {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.Arrays;
 
+public class BasicArray {
+    public static void MinAndMax() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        final int SIZE = Integer.parseInt(input.readLine());
+        String[] nums = input.readLine().split(" ");
+        int[] numbers = new int[SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            numbers[i] = Integer.parseInt(nums[i]);
+        }
+        Arrays.sort(numbers);
+
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+        output.write(numbers[0] + " " + numbers[SIZE - 1]);
+        output.flush();
+        output.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        MinAndMax();
     }
 }
