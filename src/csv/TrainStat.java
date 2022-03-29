88e56f8381ec9a7891349cfaa4d315514b373741
HJK9810 <ruby45342@gamail.com>
2022-03-29 20:16:59 +0900

    print per year

--- src/csv/TrainStat.java
+++ src/csv/TrainStat.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 public class TrainStat {
     public static void main(String[] args) {
         ArrayList<String> days = new ArrayList<>(); // seperate by date
+        ArrayList<String> Year = new ArrayList<>();
 
         ArrayList<Integer> dates = new ArrayList<>();
         ArrayList<Integer> years = new ArrayList<>();
@@ -26,11 +27,11 @@ public class TrainStat {
                 String[] ary = line.split(",");
 
                 if (ary[4].equals("승차인원수")) continue;
-                if (!days.contains(ary[0])) {
-                    days.add(ary[0]);
+                if (!days.contains(ary[0])) { // not same
+                    days.add(ary[0]); // yyyy-MM-dd
                     in.add(Integer.parseInt(ary[4]));
                     out.add(Integer.parseInt(ary[5]));
-                } else {
+                } else { // add
                     int idx = days.indexOf(ary[0]);
                     in.set(idx, in.get(idx) + Integer.parseInt(ary[4]));
                     out.set(idx, out.get(idx) + Integer.parseInt(ary[5]));
@@ -38,24 +39,28 @@ public class TrainStat {
 
                 String[] today = ary[0].split("-");
 
-                if (today[2].equals("01")) {
+                if (today[2].equals("01")) { // dates is 01
                     dates.add(count);
                     mcount += count;
                     count = 1;
                 } else count++;
 
-                if (today[1].equals("01")) {
+                if (!today[0].equals(year)) { // year change
                     years.add(mcount);
+                    Year.add(year);
                     mcount = 0;
+                    year = today[0];
                 }
-
             }
+            // for last year
+            years.add(mcount);
+            Year.add(year);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        for (int i = 0; i < years.size(); i++) {
-            System.out.println(years.get(i));
+        for (int i = 1; i < years.size(); i++) { // i = 0 -> fist line
+            System.out.println(Year.get(i) + " - " + years.get(i));
         }
 
 //        for (int i = 0; i < days.size(); i++) {
