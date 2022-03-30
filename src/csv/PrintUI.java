f47842be4128331b0679067b7af8af26f94ac25f
HJK9810 <ruby45342@gamail.com>
2022-03-30 21:00:38 +0900

    add culm UI

--- src/csv/PrintUI.java
+++ src/csv/PrintUI.java
@@ -6,18 +6,38 @@ public class PrintUI {
     Scanner input = new Scanner(System.in);
 
     public void printcheck() {
+        PrintUI pui = new PrintUI();
         System.out.println("어떠한 분류를 선택하시겠습니까?");
         System.out.println("1. 분기별\n2. 누적");
         int num = input.nextInt();
 
         while (true) {
             if (num == 1) {
-                System.out.println("분기별입니다.");
+                // 분기별 ui함수 추가
                 break;
             } else if (num == 2) {
-                System.out.println("누적별입니다.");
+                pui.printcheck();
                 break;
             } else System.out.println("다시 입력해주세요.");
+        }
+    }
+
+    public void printculm() { // 누적별
+        System.out.println("누적되고자 하는 종류를 선택해주세요");
+        System.out.println("1. 년도별\n2. 월별\n3. 정류장별");
+        int num = input.nextInt();
+
+        while(true) {
+            if(num==1) {
+                System.out.println("년도별입니다.");
+                break;
+            } else if(num==2) {
+                System.out.println("월별입니다.");
+                break;
+            } else if(num==3) {
+                System.out.println("정류장별입니다.");
+                break;
+            } else System.out.println("잘못누르셨습니다. 다시 입력해주세요.");
         }
     }
 }
