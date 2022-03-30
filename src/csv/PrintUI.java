fbf8a18f5ea3b2eac82fbed0aa1ad46df187be32
HJK9810 <ruby45342@gamail.com>
2022-03-30 20:54:18 +0900

    edit error

--- src/csv/PrintUI.java
+++ src/csv/PrintUI.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class PrintUI {
     Scanner input = new Scanner(System.in);
 
-    public String printcheck() {
+    public void printcheck() {
         System.out.println("어떠한 분류를 선택하시겠습니까?");
         System.out.println("1. 분기별\n2. 누적");
         int num = input.nextInt();
 
         while (true) {
             if (num == 1) {
-                System.out.print("분기별입니다.");
+                System.out.println("분기별입니다.");
                 break;
             } else if (num == 2) {
-                System.out.print("누적별입니다.");
+                System.out.println("누적별입니다.");
                 break;
             } else System.out.println("다시 입력해주세요.");
         }
--- src/csv/TrainStat.java
+++ src/csv/TrainStat.java
@@ -3,8 +3,10 @@ package csv;
 public class TrainStat {
     public static void main(String[] args) {
         ReadCsv rc = new ReadCsv();
+        PrintUI pui = new PrintUI();
         String path = "C:\\Users\\saiiy\\Documents\\csv\\한국철도공사_정차역별 역무열차종별 승하차인원수_20201231.csv";
 
+        pui.printcheck();
         rc.ReadFile(path);
     }
 }
