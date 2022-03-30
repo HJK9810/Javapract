6bd4dfc463dec9539a9a0aec76eb46719ba20c6c
HJK9810 <ruby45342@gamail.com>
2022-03-30 20:25:13 +0900

    seperater main and class

--- src/csv/TrainStat.java
+++ src/csv/TrainStat.java
@@ -1,70 +1,10 @@
 package csv;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-
 public class TrainStat {
     public static void main(String[] args) {
-        ArrayList<String> days = new ArrayList<>(); // seperate by date
-        ArrayList<String> Year = new ArrayList<>();
+        ReadCsv rc = new ReadCsv();
+        String path = "C:\\Users\\saiiy\\Documents\\csv\\한국철도공사_정차역별 역무열차종별 승하차인원수_20201231.csv";
 
-        ArrayList<Integer> dates = new ArrayList<>();
-        ArrayList<Integer> years = new ArrayList<>();
-        ArrayList<Integer> in = new ArrayList<>();
-        ArrayList<Integer> out = new ArrayList<>();
-
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\saiiy\\Documents\\csv\\한국철도공사_정차역별 역무열차종별 승하차인원수_20201231.csv"));
-            Charset.forName("euc-kr"); // change language type
-            String line;
-            int count = 0, ycount = 0, mcount = 0;
-            String year = "";
-            String month = "";
-            while ((line = br.readLine()) != null) {
-                String[] ary = line.split(",");
-
-                if (ary[4].equals("승차인원수")) continue;
-                if (!days.contains(ary[0])) { // not same
-                    days.add(ary[0]); // yyyy-MM-dd
-                    in.add(Integer.parseInt(ary[4]));
-                    out.add(Integer.parseInt(ary[5]));
-                } else { // add
-                    int idx = days.indexOf(ary[0]);
-                    in.set(idx, in.get(idx) + Integer.parseInt(ary[4]));
-                    out.set(idx, out.get(idx) + Integer.parseInt(ary[5]));
-                }
-
-                String[] today = ary[0].split("-");
-
-                if (today[2].equals("01")) { // dates is 01
-                    dates.add(count);
-                    mcount += count;
-                    count = 1;
-                } else count++;
-
-                if (!today[0].equals(year)) { // year change
-                    years.add(mcount);
-                    Year.add(year);
-                    mcount = 0;
-                    year = today[0];
-                }
-            }
-            // for last year
-            years.add(mcount);
-            Year.add(year);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        for (int i = 1; i < years.size(); i++) { // i = 0 -> fist line
-            System.out.println(Year.get(i) + " - " + years.get(i));
-        }
-
-//        for (int i = 0; i < days.size(); i++) {
-//            System.out.println(days.get(i) + " - 승차 : " + in.get(i) + ", 하차 : " + out.get(i));
-//        }
+        rc.ReadFile(path);
     }
 }
