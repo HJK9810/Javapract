c2ede1702fb84e72614dbf53d037c6a0a1c2c961
HJ_Kim <ruby45342@gmail.com>
2022-08-18 10:38:00 +0900

    문자열 내마음대로 정렬

--- src/programmers/Pract.java
+++ src/programmers/Pract.java
@@ -92,6 +92,21 @@ public class Pract {
         return answer;
     }
 
+    private String[] mySortRule(String[] strings, int n) {
+        List<String> list = new ArrayList<>();
+        String[] answer = new String[strings.length];
+
+        for(String word : strings) {
+            list.add(word.charAt(n) + word);
+        }
+        Collections.sort(list);
+        for(int i=0;i<list.size();i++) {
+            answer[i] = list.get(i).substring(1);
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         Pract pract = new Pract();
 
