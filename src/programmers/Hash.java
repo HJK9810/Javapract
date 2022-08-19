34f082ebba033d5874349281beed7f8f330dad03
HJ_Kim <ruby45342@gmail.com>
2022-08-19 10:02:06 +0900

    완주하지 못한 선수

--- src/programmers/Hash.java
+++ src/programmers/Hash.java
@@ -5,13 +5,23 @@ import java.util.*;
 public class Hash {
     private int PoketMon(int[] nums) {
         Set<Integer> set = new HashSet<Integer>();
-        for(int num : nums) set.add(num);
+        for (int num : nums) set.add(num);
         int count = set.size();
 
         return nums.length / 2 <= count ? nums.length / 2 : count;
     }
 
-    public static void main(String[] args) {
+    private String NotComplete(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
 
+        for (int i = 0; i < participant.length; i++) {
+            if (!participant[i].equals(completion[i])) return participant[i];
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Hash hash = new Hash();
     }
 }
