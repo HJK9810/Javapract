75b0273775be92a33b114fbe84a66de46f573475
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-08-21 10:54:41 +0900

    DevMatching 21-로또최고,최저순위

---
+++ src/programmers/DebMatch/WebBack21.java
@@ -0,0 +1,45 @@
+package programmers.DebMatch;
+
+import java.util.*;
+
+public class WebBack21 {
+    private int checkwin(int num) {
+        switch (num) {
+            case 2:
+                return 5;
+            case 3:
+                return 4;
+            case 4:
+                return 3;
+            case 5:
+                return 2;
+            case 6:
+                return 1;
+            default:
+                return 6;
+        }
+    }
+
+    private int[] CheckLottes(int[] lottos, int[] win_nums) {
+        int count = 0;
+        int zero = 0;
+        Arrays.sort(lottos);
+
+        for (int i = 0; i < lottos.length; i++) {
+            if (lottos[i] == 0) zero = i + 1;
+            else {
+                if (Arrays.asList(win_nums).contains(lottos[i])) count++;
+            }
+        }
+
+        return new int[]{checkwin(count + zero), checkwin(count)};
+    }
+
+    public static void main(String[] args) {
+        WebBack21 webBack21 = new WebBack21();
+
+        System.out.println(webBack21.CheckLottes(new int[]{44, 1, 0, 0, 31, 25}, new int[]{31, 10, 45, 1, 6, 19}).equals(new int[]{3, 5}));
+        System.out.println(webBack21.CheckLottes(new int[]{0, 0, 0, 0, 0, 0}, new int[]{38, 19, 20, 40, 15, 25}).equals(new int[]{1, 6}));
+        System.out.println(webBack21.CheckLottes(new int[]{45, 4, 35, 20, 3, 9}, new int[]{20, 9, 3, 45, 4, 35}).equals(new int[]{1, 1}));
+    }
+}
