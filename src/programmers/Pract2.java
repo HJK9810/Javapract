b909e6981c603df2472e59b5768a6e41a308527e
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-09-05 19:38:22 +0900

    하노이의탑

--- src/programmers/Pract2.java
+++ src/programmers/Pract2.java
@@ -1,5 +1,9 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Pract2 {
     private static int[] dp;
 
@@ -19,13 +23,30 @@ public class Pract2 {
         return fibo(n);
     }
 
+    List<int[]> moves = new ArrayList<>();
+
+    private void hanoi(int n, int from, int by, int to) {
+        if (n == 0) return;
+        hanoi(n - 1, from, to, by);
+        moves.add(new int[]{from, to});
+        hanoi(n - 1, by, from, to);
+    }
+
+    private int[][] TowerOfHanoi(int n) {
+        hanoi(n, 1, 2, 3);
+        return moves.toArray(new int[moves.size()][2]);
+    }
+
 
     // level2 연습문제
     public static void main(String[] args) {
         Pract2 pract2 = new Pract2();
 
         // 2 x n 타일링
-        System.out.println(pract2.Nx2Tiling(4));
-        System.out.println(pract2.Nx2Tiling(4) == 5);
+//        System.out.println(pract2.Nx2Tiling(4));
+//        System.out.println(pract2.Nx2Tiling(4) == 5);
+
+        // 하노이의 탑
+        System.out.println(Arrays.toString(pract2.TowerOfHanoi(2)));
     }
 }
