a15230b5713f1251006c8a302e42d6e9b0317ff1
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-09-07 19:49:16 +0900

    수 정렬하기2

--- src/backjoon/BasicSort.java
+++ src/backjoon/BasicSort.java
@@ -20,6 +20,24 @@ public class BasicSort {
         }
     }
 
+    private void Sorting2() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        final int SIZE = Integer.parseInt(input.readLine());
+        List<Integer> sort = new ArrayList<>();
+
+        for (int i = 0; i < SIZE; i++) {
+            sort.add(Integer.parseInt(input.readLine()));
+        }
+
+        Collections.sort(sort);
+
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int num : sort) {
+            output.write(num + "\n");
+        }
+        output.flush();
+        output.close();
+    }
 
     public static void main(String[] args) throws IOException {
         BasicSort basicSort = new BasicSort();
