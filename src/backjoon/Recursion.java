da5ba0b4f0e6284a16dc06cb22907343f8e40109
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-09-09 14:18:01 +0900

    재귀함수가 뭔가요

--- src/backjoon/Recursion.java
+++ src/backjoon/Recursion.java
@@ -30,10 +30,40 @@ public class Recursion {
         output.close();
     }
 
+    static String underbar = "";
+
+    private static void strRE(int n) throws IOException {
+        String line = underbar;
+        StringBuffer sb = new StringBuffer();
+
+        if (n == 0) {
+            System.out.println(line + "\"재귀함수가 뭔가요?\"");
+            System.out.println(line + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+            System.out.println(line + "라고 답변하였지.");
+            return;
+        }
+        System.out.println(line + "\"재귀함수가 뭔가요?\"");
+        System.out.println(line + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+        System.out.println(line + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+        System.out.println(line + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+
+        underbar += "____";
+        strRE(n - 1);
+        System.out.println(line + "라고 답변하였지.");
+    }
+
+    private void Recurs() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(input.readLine());
+        System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+        strRE(num);
+    }
+
     public static void main(String[] args) throws IOException {
         Recursion recursion = new Recursion();
 
 //        recursion.Pactorial(); // 팩토리얼
-        recursion.Fibonacci(); // 피보나치 수5
+//        recursion.Fibonacci(); // 피보나치 수5
+        recursion.Recurs();
     }
 }
