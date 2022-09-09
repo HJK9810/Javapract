e5dfac4cba969cd28633c65c0949af2af0179d9d
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-09-09 09:28:46 +0900

    팩토리얼

--- src/backjoon/Recursion.java
+++ src/backjoon/Recursion.java
@@ -1,7 +1,24 @@
 package backjoon;
 
-public class Recursion {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Recursion {
+    private static int pact(int n) {
+        if (n > 1) return n * pact(n - 1);
+        else return 1;
+    }
+
+    private void Pactorial() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+        output.write(pact(Integer.parseInt(input.readLine())) + "\n");
+        output.flush();
+        output.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Recursion recursion = new Recursion();
+
+        recursion.Pactorial(); // 팩토리얼
     }
 }
