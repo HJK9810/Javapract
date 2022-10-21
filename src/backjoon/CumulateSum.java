dd5769254853800899cdffd14855b60d0ce0541f
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-10-21 11:08:53 +0900

    수열

--- src/backjoon/CumulateSum.java
+++ src/backjoon/CumulateSum.java
@@ -26,9 +26,34 @@ public class CumulateSum {
         output.close();
     }
 
+    private void Sequence() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String[] leng = input.readLine().split(" ");
+        final int SIZE = Integer.parseInt(leng[0]);
+        final int section = Integer.parseInt(leng[1]);
+        String[] numbers = input.readLine().split(" ");
+
+        int[] numsSum = new int[SIZE + 1];
+        numsSum[0] = 0;
+        for (int i = 0; i < numsSum.length - 1; i++) {
+            numsSum[i + 1] = numsSum[i] + Integer.parseInt(numbers[i]);
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i < SIZE - section + 2; i++) {
+            int sum = numsSum[i + section - 1] - numsSum[i - 1];
+            if (sum > max) max = sum;
+        }
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+        output.write(max + "\n");
+        output.flush();
+        output.close();
+    }
+
     public static void main(String[] args) throws IOException {
         CumulateSum cumulateSum = new CumulateSum();
 
-        cumulateSum.SectionSum(); // 구간합 구하기4
+//        cumulateSum.SectionSum(); // 구간합 구하기4
+        cumulateSum.Sequence(); // 수열
     }
 }
