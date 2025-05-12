e5446be4b64645b7878064b08a0fd77719fca903
HJK9810 <ruby45342@gmail.com>
2025-05-12 19:08:33 +0900

    use dp

--- src/backjoon/Fourth.java
+++ src/backjoon/Fourth.java
@@ -1,36 +1,22 @@
 package backjoon;
 
 import java.io.*;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Fourth {
-    private void checkOne(int base, int count, Set<Integer> list) {
-        if (base == 1) {
-            list.add(count);
-            return;
-        }
-
-        if (base % 3 == 0) {
-            checkOne(base / 3, count + 1, list);
-        }
-        if (base % 2 == 0) {
-            checkOne(base / 2, count + 1, list);
-        }
-        checkOne(base - 1, count + 1, list);
-    }
-
     private void changedOne() throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
         int number = Integer.parseInt(input.readLine());
-        Set<Integer> list = new HashSet<>();
+        int[] count = new int[number + 1];
 
-        checkOne(number, 0, list);
-        int min = Collections.min(list);
+        count[1] = 0;
+        for (int idx = 2; idx <= number; idx++) {
+            count[idx] = count[idx - 1] + 1;
+            if (idx % 2 == 0) count[idx] = Math.min(count[idx], count[idx / 2] + 1);
+            if (idx % 3 == 0) count[idx] = Math.min(count[idx], count[idx / 3] + 1);
+        }
 
-        System.out.println(min);
+        System.out.println(count[number]);
     }
 
     public static void main(String[] args) throws IOException {
