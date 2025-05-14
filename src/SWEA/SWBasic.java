d722109a45b564546e74e3e2e136ed29739ec6bf
HJK9810 <ruby45342@gmail.com>
2025-05-14 20:45:58 +0900

    1209 S/W 문제해결 기본 Sum

--- src/SWEA/SWBasic.java
+++ src/SWEA/SWBasic.java
@@ -1,6 +1,7 @@
 package SWEA;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class SWBasic {
     private void View_OneDay() throws Exception {
@@ -31,10 +32,52 @@ public class SWBasic {
         System.out.println(sb);
     }
 
-    public static void main(String args[]) throws Exception
+    private void maxSum() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        for(int test_case = 1; test_case <= 10; test_case++){
+            int num = Integer.parseInt(input.readLine());
+            int[][] grid = new int[100][100];
+
+            for (int i = 0; i < grid.length; i++) {
+                String[] inputAry = input.readLine().split(" ");
+                for (int j = 0; j < grid[0].length; j++) {
+                    grid[i][j] = Integer.parseInt(inputAry[j]);
+                }
+            }
+
+            int maxColSum = Integer.MIN_VALUE;
+            int crossSum = 0;
+            int reversedSum = 0;
+            int[] rowSums = new int[100];
+
+            for (int index = 0; index < grid.length; index++) {
+                int colSum = 0;
+                for (int col = 0; col < grid[index].length; col++) {
+                    colSum += grid[index][col];
+                    rowSums[col] += grid[index][col];
+                }
+                crossSum += grid[index][index];
+                reversedSum += grid[99 - index][99 - index];
+                maxColSum = Math.max(maxColSum, colSum);
+            }
+            int maxCross = Math.max(reversedSum, crossSum);
+            int maxLine = Math.max(maxColSum, maxCross);
+
+            for (int row = 0; row < rowSums.length; row++) {
+                maxLine = Math.max(maxLine, rowSums[row]);
+            }
+            sb.append(String.format("#%d %d\n", test_case, maxLine));
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String args[]) throws IOException
     {
         SWBasic basic = new SWBasic();
 
-        basic.View_OneDay();
+//        basic.View_OneDay();
+        basic.maxSum();
     }
 }
