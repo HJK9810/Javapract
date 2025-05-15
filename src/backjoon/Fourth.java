4545833214a91424cd430a295c0caad15f05e84e
HJK9810 <ruby45342@gmail.com>
2025-05-15 16:55:25 +0900

    12865 평범한 배낭 : Fail

--- src/backjoon/Fourth.java
+++ src/backjoon/Fourth.java
@@ -3,6 +3,8 @@ package backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Fourth {
     private void fixAtoB() throws IOException {
@@ -36,9 +38,50 @@ public class Fourth {
         System.out.println(isImpossibleValue ? -1 : count + 1);
     }
 
+    private void CommonBag() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] inputvalue = input.readLine().split(" ");
+        final int SIZE = Integer.parseInt(inputvalue[0]);
+        final int amount = Integer.parseInt(inputvalue[1]);
+
+        int maxValue = Integer.MIN_VALUE;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] keys = new int[SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            String[] value = input.readLine().split(" ");
+            map.put(Integer.parseInt(value[0]), Integer.parseInt(value[1]));
+            keys[i] = Integer.parseInt(value[0]);
+        }
+
+        Arrays.sort(keys);
+        for (int idx = SIZE - 1; idx >= 0; idx--) {
+            int key = keys[idx];
+            int value = map.get(key);
+
+            if (amount < key) continue;
+
+            int need = amount - key;
+            if (need < keys[0]) {
+                maxValue = Math.max(maxValue, value);
+                continue;
+            }
+
+            int index = 0;
+            while (index < SIZE && need >= keys[index]) {
+                index++;
+            }
+            maxValue = Math.max(maxValue, value + (index == 0 ? 0 : map.get(keys[index - 1])));
+        }
+
+        System.out.println(maxValue);
+    }
+
     public static void main(String[] args) throws IOException {
         Fourth fourth = new Fourth();
 
-        fourth.fixAtoB();
+//        fourth.fixAtoB();
+        fourth.CommonBag();
     }
 }
