558a20d4778f9a640a2ddd2eb3665aa97fa08813
HJK9810 <ruby45342@gmail.com>
2025-05-21 18:24:33 +0900

    1806 부분합

--- src/backjoon/Fifth.java
+++ src/backjoon/Fifth.java
@@ -29,9 +29,39 @@ public class Fifth {
         System.out.println(String.format("%.1f", Math.abs(sum / 2.0)));
     }
 
+    private void PartialSum() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] line = input.readLine().split(" ");
+        final int SIZE = Integer.parseInt(line[0]);
+        final int S = Integer.parseInt(line[1]);
+        int[] partial = new int[SIZE];
+
+        String[] numbers = input.readLine().split(" ");
+        for (int index = 0; index < SIZE; index++) {
+            partial[index] = Integer.parseInt(numbers[index]);
+        }
+
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        while (right < SIZE) {
+            sum += partial[right++];
+
+            while (sum >= S) {
+                minLength = Math.min(minLength, right - left);
+                sum -= partial[left++];
+            }
+        }
+        System.out.println(minLength == Integer.MAX_VALUE ? 0 : minLength);
+    }
+
     public static void main(String[] args) throws IOException {
         Fifth fifth = new Fifth();
 
-        fifth.polygonArea();
+//        fifth.polygonArea();
+        fifth.PartialSum();
     }
 }
