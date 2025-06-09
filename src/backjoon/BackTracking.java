5f655ccdc71913878cdcb9ab9c93937939dcc4a4
HJK9810 <ruby45342@gmail.com>
2025-06-09 19:53:06 +0900

    15654 N과 M(5)

--- src/backjoon/BackTracking.java
+++ src/backjoon/BackTracking.java
@@ -3,8 +3,10 @@ package backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class BackTracking {
+    private static boolean[] visited;
     private static StringBuilder sb = new StringBuilder();
 
     private void permutation(int[] arr, int[] out, int depth, int r) {
@@ -22,6 +24,25 @@ public class BackTracking {
         }
     }
 
+    private void combination(int[] base, int[] out, int depth, int r) {
+        if (depth == r) {
+            for (int num : out) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int num = 0; num < base.length; num++) {
+            if (!visited[num]) {
+                visited[num] = true;
+                out[depth] = base[num];
+                combination(base, out, depth + 1, r);
+                visited[num] = false;
+            }
+        }
+    }
+
     private void NandM3() throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         sb = new StringBuilder();
@@ -36,6 +57,27 @@ public class BackTracking {
         }
 
         permutation(numbers, new int[depth], 0, depth);
+        System.out.print(sb);
+    }
+
+    private void NandM5() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        String[] line = input.readLine().split(" ");
+        final int SIZE = Integer.parseInt(line[0]);
+        final int depth = Integer.parseInt(line[1]);
+
+        int[] base = new int[SIZE];
+        String[] strBase = input.readLine().split(" ");
+
+        for (int i = 0; i < SIZE; i++) {
+            base[i] = Integer.parseInt(strBase[i]);
+        }
+        Arrays.sort(base);
+
+        visited = new boolean[SIZE];
+        combination(base, new int[depth], 0, depth);
         System.out.print(sb);
     }
 
