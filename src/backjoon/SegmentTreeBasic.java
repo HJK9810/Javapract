dcfb56281f09db2a2e8d2cab5b1ac11a8117f5b3
HJK9810 <ruby45342@gmail.com>
2025-08-07 15:04:53 +0900

    14427 수열과 쿼리16

--- src/backjoon/SegmentTreeBasic.java
+++ src/backjoon/SegmentTreeBasic.java
@@ -39,6 +39,21 @@ public class SegmentTreeBasic {
         return (int) tree[node];
     }
 
+    private int queryIndex(int left, int right, int node, int start, int end) {
+        if (right < start || end < left) return -1;
+        if (left <= start && end <= right) return (int) tree[node];
+
+        int mid = (start + end) / 2;
+        int leftIdx = queryIndex(left, right, node * 2, start, mid);
+        int rightIdx = queryIndex(left, right, node * 2 + 1, mid + 1, end);
+
+        if (leftIdx == -1) return rightIdx;
+        if (rightIdx == -1) return leftIdx;
+
+        if (initial[leftIdx] == initial[rightIdx]) return Math.min(leftIdx, rightIdx);
+        return initial[leftIdx] < initial[rightIdx] ? leftIdx : rightIdx;
+    }
+
     private void SequenceAndQuery() throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -71,9 +86,45 @@ public class SegmentTreeBasic {
         System.out.println(sb);
     }
 
+    private void SequenceAndQuery16() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        final int SIZE = Integer.parseInt(input.readLine());
+        initial = new long[SIZE];
+
+        String[] ary = input.readLine().split(" ");
+        for (int idx = 0; idx < SIZE; idx++) {
+            initial[idx] = Long.parseLong(ary[idx]);
+        }
+
+        tree = new long[SIZE * 4];
+        initIndex(1, 0, SIZE - 1);
+
+        final int questSize = Integer.parseInt(input.readLine());
+        for (int count = 0; count < questSize; count++) {
+            String[] line = input.readLine().split(" ");
+            String order = line[0];
+
+            if (order.equals("1")) {
+                int index = Integer.parseInt(line[1]) - 1;
+                int value = Integer.parseInt(line[2]);
+                initial[index] = value;
+                updateIndex(index, value, 1, 0, SIZE - 1);
+            } else {
+                int start = Integer.parseInt(line[1]) - 1;
+                int end = Integer.parseInt(line[2]) - 1;
+                int minIndex = queryIndex(start, end, 1, 0, SIZE - 1);
+                sb.append(minIndex + 1).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+
     public static void main(String[] args) throws IOException {
         SegmentTreeBasic basic = new SegmentTreeBasic();
 
-        basic.SequenceAndQuery();
+//        basic.SequenceAndQuery();
+        basic.SequenceAndQuery16();
     }
 }
